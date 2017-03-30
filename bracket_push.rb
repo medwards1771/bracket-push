22b57c91afbbd91814d3601f4e5afc41a285e7f0
Meredith Edwards <meredith.edwards1771@gmail.com>
2017-03-29 22:24:53 -0400

    First commit

---
+++ bracket_push.rb
@@ -0,0 +1,27 @@
+class Brackets
+  def self.paired?(string)
+    return true if string == ''
+    return false unless leading_character?(string[0])
+    p "string=#{string} #{matching_brackets(string)}"
+    return true if matching_brackets(string)
+  end
+
+  def self.leading_character?(char)
+    char == '(' || char == '{' || char == '['
+  end
+
+  def self.matching_brackets(string)
+    if string.include?('{')
+      string.count('{') == string.count('}')
+    end
+    if string.include?('[')
+      string.count('[') == string.count(']')
+    end
+    if string.include?('(') || string.include?(')')
+      string.count('(') == string.count(')')
+    end
+    if string.include?('{') && string.include?('[')
+      string.count('{') == string.count('}') && string.count('[') == string.count(']')
+    end
+  end
+end
